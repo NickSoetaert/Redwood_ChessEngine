222e4117e9a111556569a211c032c620613928cf
Seaney Shell <afc76927@opayq.com>
2019-04-14 00:14:14 -0700

    reverted to showcase version

--- AI.java
+++ AI.java
@@ -1,21 +1,14 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-
-
-
 public class AI
 {
     public Board currentGame;
     public float WhiteTime,BlackTime;
-    public Color ourColor;
     
-    
-
-    public AI(Board currentGame, Color ourColor)
+    public AI(Board currentGame)
     {
         this.currentGame = currentGame;
-        this.ourColor = ourColor;
     }
 
     public String Think(ArrayList<String> moves)
@@ -29,35 +22,6 @@ public class AI
     public void act(String action)
     {
         currentGame.move(action);
-    }
-
-
-
-
-
-    public static int minmaxWhiteCalc(Board board, Color colorCheck, int depth, int MaxDepth, ArrayList<String> History)
-    {
-        int score = board.eval();
-
-        if (depth >= MaxDepth)
-        {
-            return score;
-        }
-
-        if (depth % 2 == 0)
-        {
-            int best = -1000;
-            ArrayList<String> childrenBoard = MoveGenerator.possibleWhiteMoves(board, "Replace me with real data");
-
-        }
-        else
-        {
-            int best = 1000;
-
-        }
-
-
-        ArrayList<String> children = MoveGenerator.possibleWhiteMoves(board, pastMove)
     }
 
 
